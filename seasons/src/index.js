d8343c8758357a3d3c371bbb9b62221958123366
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-25 11:56:44 -0400

    add functional component and reactDOM function

---
+++ seasons/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// functional component
+const App = () => {
+    return <div>Hi there!</div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);

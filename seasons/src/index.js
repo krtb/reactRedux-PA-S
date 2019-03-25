000881523e450f0e4718aaed02cae616ec3136c0
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-25 12:18:21 -0400

    add geolocation callback functions, for success and failure

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 
 // functional component
 const App = () => {
+
+    // need to pass in 2 seperate function callbacks
+    window.navigator.geolocation.getCurrentPosition(
+
+        // first func called when location success
+        // success Callback
+        (position) => console.log(position, 'SUCCESS'),
+        // fauilure callback, unable to determine
+        (err) => console.log(err, 'FAILURE')
+
+    );
+    
     return <div>Hi there!</div>
 }
 

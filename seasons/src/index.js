33ed78bf23843985f6acbb24c66d65a272f19100
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-26 12:23:36 -0400

    adding notes

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
 
     render() {
-
+        // User's Current Latitude is single piece of state that we care about here:
+        
         // need to pass in 2 seperate function callbacks
         window.navigator.geolocation.getCurrentPosition(
 

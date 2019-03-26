e23b0683176ea31d0725121691599346945e7456
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-26 12:40:51 -0400

    adding notes

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
+    // initialized with props object
+    constructor(props) {
+        // have to call super, pass in props
+        // extending, borrowing functionality
+        // defnining here replaces/overrides constructor func
+        // inside of React.Component class
+        // to make sure parent gets called, call SUPER w/ props
+        super(props)
+
+    }
+
     render() {
         // User's Current Latitude is single piece of state that we care about here:
-        
+
         // need to pass in 2 seperate function callbacks
         window.navigator.geolocation.getCurrentPosition(
 

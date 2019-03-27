5272737a4d24e8b23efcecfa318221c74727c2cc
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-27 11:08:50 -0400

    clean up comments

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -20,22 +20,9 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        // User's Current Latitude is single piece of state that we care about here:
-
-        // need to pass in 2 seperate function callbacks
         window.navigator.geolocation.getCurrentPosition(
-
-            // first func called when location success
-            // success Callback
-            position => {
-                // to update state object, called setState()
-                this.setState({ lat: position.coords.latitude })
-            },
-            // fauilure callback, unable to determine
-            err => {
-                this.setState({ errorMessage: err.message })
-            }
-
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 

f71e0e684a7b402b0eee648b66afabd60bc441b4
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-27 09:45:32 -0400

    move fetching of location up to constructor method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,9 +17,6 @@ class App extends React.Component {
             lat: null,
         };
 
-    }
-
-    render() {
         // User's Current Latitude is single piece of state that we care about here:
 
         // need to pass in 2 seperate function callbacks
@@ -32,6 +29,11 @@ class App extends React.Component {
             (err) => console.log(err)
 
         );
+
+    }
+
+    render() {
+
         return <div>Lattitude: {this.state.lat}</div>;
     }
 

1ebb7360c947fd08b0a0f86cd7d7c1a3cb006f37
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-27 11:55:46 -0400

    add semantic ui icons to be displayed by helper variable, on ternary being true

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -12,13 +12,16 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {  
   // helper variable
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Burr, it is chilly!" : "ooof, it's is hot!";;
+  const text = season === 'winter' ? 'Burr, it is chilly! ': 'ooof, it\'s is hot!';
+  const icon = season === 'winter' ? 'snowflake' : 'sun'
 
   return (
     <div>
-      { text }
-    </div>  
-    );
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;

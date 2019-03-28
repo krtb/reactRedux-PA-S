0ed4a03ea7238bb930a930cb17f1ba3721fc2a80
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-27 23:58:47 -0400

    add functional component for spinner loading page

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+            <div class="ui active dimmer">
+                <div class="ui big text loader">Loading</div>
+                <p>Loading...</p>
+            </div>
+    );
+}
+
+export default Spinner;

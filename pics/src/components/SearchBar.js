0394a20890a5d75e8d4345f9d2158a8f0c39c58d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-29 16:26:26 -0400

    add callback function to refrence event handler func above

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  // event handler, callback function
+  onInputChange() {
+
+  }
+
     render(){
         return (
           <div className="ui segment">
             <form className="ui form">
               <div className="field">
                 <label>Image Search</label>
-                <input type="text" />
+                {/* onInputChange not with (), or would be called every time component rendered */}
+                <input type="text"  onChange={this.onInputChange}/>
               </div>
             </form>
           </div>

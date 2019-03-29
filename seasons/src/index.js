c9227a1559ad8f5ab87e33eb84abd592093bc57c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-29 00:44:02 -0400

    add helper function that will always return some element

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,18 +17,32 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent(){
         // conditional rendering below
-        if(this.state.errorMessage && !this.state.lat){
+        if (this.state.errorMessage && !this.state.lat) {
             return <div> Error: {this.state.errorMessage} </div>
         }
 
         if (!this.state.errorMessage && this.state.lat) {
             // passing down state as props to component
-            return <SeasonDisplay lat={this.state.lat}/>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return <Spinner message="Please accept location request" />
+    }
+
+    render() {
+
+        // any time we make a component, want it to not have as many returns for exacty this reason
+        // condition where no matter what, always want to return what's wrapped inside of a copmonent
+        // with some common element
+        return(
+            <div className=" border red ">
+                {/* no matter what the result, will always surround with item that has red border */}
+                {this.renderContent()}
+            </div>
+        )
+
     }
 
 }

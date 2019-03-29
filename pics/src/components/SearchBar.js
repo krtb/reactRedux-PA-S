2f72145dd2bee717c53cec0639ed3b801827a2d7
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-29 16:04:18 -0400

    create form input field and stule with semantic classes

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 class SearchBar extends React.Component {
     render(){
-        return(
-            <div>
-                <form>
-                    <input type="text" />
-                </form>
-            </div>
+        return (
+          <div className="ui segment">
+            <form className="ui form">
+              <div className="field">
+                <label>Image Search</label>
+                <input type="text" />
+              </div>
+            </form>
+          </div>
         );
     }
 }

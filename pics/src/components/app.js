22a2825a9fa6002176d81400f208b2b2953bf48d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-29 15:32:01 -0400

    create SearchBar class instance

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
+// functional component
 const App = () => {
-    return <div>HELLO THERE</div>
-}
+    return (
+        <div>
+            {/* import searchbar to be rendered in index */}
+            <SearchBar/>
+        </div>
+    );
+};
 
 export default App;
+

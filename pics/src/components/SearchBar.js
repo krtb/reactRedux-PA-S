bbe8c1c74e27ac7adaeb5c69494dbf70d86a6aab
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-30 23:49:43 -0400

    add event handler that will tricker onSubmit callBack function

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -6,19 +6,19 @@ class SearchBar extends React.Component {
     term: '',
   }
 
-  // event handler, callback function
-  // onInputChange(e) {
-  //   console.log(e.target.value);
-  // }
+  onSubmit = () => {
+
+  }
 
     render(){
         return (
           <div className="ui segment">
-            <form className="ui form">
+          {/* adding event handler that will trigger callBack function: don't want page to refresg every time user presses enter. 
+              Instead want to pass in some custom logic of our own */}
+            <form onSubmit={this.onSubmit} className="ui form">
               <div className="field">
                 <label>Image Search</label>
-                {/* take input value, force to uppercase each time filtered in, replace input value with transformed text */}
-                <input type="text" value={this.state.term}  onChange={e => this.setState({term: e.target.value.toUpperCase() })}/>
+                <input type="text" value={this.state.term}  onChange={e => this.setState({term: e.target.value })}/>
               </div>
             </form>
           </div>

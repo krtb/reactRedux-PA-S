f257bd03d64cdbec085b74e53e07c00a0ada8b32
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-30 23:51:00 -0400

    change name of callBack function to fit convention

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -6,7 +6,7 @@ class SearchBar extends React.Component {
     term: '',
   }
 
-  onSubmit = () => {
+  onFormSubmit = () => {
 
   }
 
@@ -15,7 +15,7 @@ class SearchBar extends React.Component {
           <div className="ui segment">
           {/* adding event handler that will trigger callBack function: don't want page to refresg every time user presses enter. 
               Instead want to pass in some custom logic of our own */}
-            <form onSubmit={this.onSubmit} className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
               <div className="field">
                 <label>Image Search</label>
                 <input type="text" value={this.state.term}  onChange={e => this.setState({term: e.target.value })}/>

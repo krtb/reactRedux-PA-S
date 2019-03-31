50e12bcb1f20fe4c30d3cd291febc5025c932852
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-31 01:37:40 -0400

    transform functional component to class component, add callback funtion to SearchBar component

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -2,14 +2,22 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 // functional component
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            {/* import searchbar to be rendered in index */}
-            <SearchBar/>
-        </div>
-    );
-};
+class App extends React.Component {
+    // callback to be passed to SearchBard component
+    // Parent component should worry about the logic
+    onSearchSubmit = (term) => {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                {/* onSearchSubmit is a refrence to callback function defined above, should not be invoked and run immediately */}
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    };
+}
 
 export default App;
 

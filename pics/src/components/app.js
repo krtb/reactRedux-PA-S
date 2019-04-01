1937df4d38719ed392452c7467a892c85c7d013c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-01 13:26:28 -0400

    clean up comments, fallback to Async Await method of handling fetch

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -6,26 +6,18 @@ require("dotenv").config();
 
 // functional component
 class App extends React.Component {
-    // ASYNCHRONUS REQUEST: will take some amount of time for
-    // axios to send request
-    // for API to send a response back
-    // => but time we get reponse back, will have exited onSearchSubmit method
+    // ASYNC AWAIT SYNTAX
     async onSearchSubmit(term) {
         const response = await axios
           .get("https://api.unsplash.com/search/photos", {
             params: {
-              // query=cars --> would be the end result, takes in value from above
               query: term
             },
             headers: {
               Authorization: `Client-ID ${process.env.REACT_APP_MYAPIKEY}`
             }
           })
-          console.log(response.data.results );
-          
-          // .then(resp => {
-          //   console.log(resp.data.results, "HERE IS MY PROMISE");
-          // }); 
+          // console.log(response.data.results );
     }
 
     render() {

5c26851e705aa59ebd2702703a4d40b6d2f9dea0
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-01 13:43:52 -0400

    refactor as arrow function to bind this to correct instance of app

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -6,8 +6,14 @@ require("dotenv").config();
 
 // functional component
 class App extends React.Component {
+  // when expecting an array/object default= empty [] / {}
+  state = {
+    images: [],
+  }
+          
     // ASYNC AWAIT SYNTAX
-    async onSearchSubmit(term) {
+    // need to move over the async keyword when using arrow function to bind this
+    onSearchSubmit = async (term) => {
         const response = await axios
           .get("https://api.unsplash.com/search/photos", {
             params: {
@@ -17,7 +23,11 @@ class App extends React.Component {
               Authorization: `Client-ID ${process.env.REACT_APP_MYAPIKEY}`
             }
           })
-          // console.log(response.data.results );
+          // props object: passed down to SEARCHBARD COMPONENT INSTANCE
+          console.log(this);
+          this.setState({
+            images: response.data.results
+          })
     }
 
     render() {
@@ -25,6 +35,7 @@ class App extends React.Component {
             <div className="ui container" style={{marginTop: '10px'}}>
                 {/* onSearchSubmit is a refrence to callback function defined above, should not be invoked and run immediately */}
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     };

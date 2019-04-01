fc96ebd3d1f1dc37cfa0489d8e6f9d6468274bc8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-01 00:59:29 -0400

    write fetch using 3rd party, Axios. Getting 200 status, successful API request

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -8,7 +8,21 @@ class App extends React.Component {
     // callback to be passed to SearchBard component
     // Parent component should worry about the logic
     onSearchSubmit = (term) => {
-        
+        // takes 2 seperate args
+        // 1 - the address that we want to make a GET request to
+        // 2 - will have options that allow us to custmize this request/ identify ourselves, who is trying to access the API
+        // end point that will allow us to search for photos = search/photos
+        // adding in a header of authorization
+        axios.get("https://api.unsplash.com/search/photos", {
+          params: {
+            // query=cars --> would be the end result, takes in value from above
+            query: term
+          },
+          headers: {
+            Authorization:
+              "Client-ID ACCESSKEYDIGITSGOHEREnotstoringforsecurityreasons"
+          }
+        });
     }
 
     render() {

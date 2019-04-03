382d8a9c38033d58ccb3257ede7b3109fa271590
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-03 00:29:41 -0400

    img tags need an alt prop

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -6,7 +6,9 @@ const ImageList = (props) => {
     // going to map over our list of images
     const images = props.images.map((image)=>{
         // when creating lists in react, they have to have a unique key 
-        return <img key={image.id} src={image.urls.regular} />
+        return (
+          <img key={image.id} alt={image.id} src={image.urls.regular} />
+        );
     });
 
 

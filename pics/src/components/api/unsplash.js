3ea9cc79ec44ff4eefaaf107ed19709bd85f14e1
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-02 23:24:30 -0400

    add export default to dumb component

--- pics/src/components/api/unsplash.js
+++ pics/src/components/api/unsplash.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 // creates instance of axios client with a couple of defaulted properties
 // .create allows us to make a customized little copy of it
-axios.create({
+export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization: `Client-ID ${process.env.REACT_APP_MYAPIKEY}`

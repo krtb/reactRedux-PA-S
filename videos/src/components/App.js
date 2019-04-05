b5fc467d03c1905b78e70d8955e0da8101d37322
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-05 16:55:20 -0400

    create folder to house components

---
+++ videos/src/components/App.js
@@ -0,0 +1,14 @@
+// app should be a class based component
+// will hold al of the different state for our Application
+import React from 'react';
+
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>HELLO THERE!</div>
+        );
+    }
+}
+
+export default App

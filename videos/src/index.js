5388cf74b591f8170c6611c1632e9e5609892edc
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-05 16:55:53 -0400

    import necessary files and add ReactDOM function

---
+++ videos/src/index.js
@@ -0,0 +1,9 @@
+// add default imports that belong in src/index
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);

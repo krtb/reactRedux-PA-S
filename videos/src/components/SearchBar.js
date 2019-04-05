5aedd1368403d1af3124919fd3462134cc6021b4
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-05 17:06:05 -0400

    add searchbar class component

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    render(){
+        return(
+            <div>SEARCH BAR COMPONENT</div>
+        );
+    }
+}
+
+export default SearchBar

78cc5d11556548d6b7863042d6e0b32f743a5d59
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-05 17:06:46 -0400

    import searchbar component and render to page

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,12 +1,13 @@
 // app should be a class based component
 // will hold al of the different state for our Application
 import React from 'react';
+import SearchBar from './SearchBar'
 
 
 class App extends React.Component{
     render() {
         return (
-            <div>HELLO THERE!</div>
+            <div><SearchBar/></div>
         );
     }
 }

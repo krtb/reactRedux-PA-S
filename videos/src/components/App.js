f2cfc794ddff8fe87331658336ced5be6c4896b9
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-07 22:59:12 -0400

    change arrow funciton to async syntax

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -5,15 +5,20 @@ import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 
 class App extends React.Component{
-    //TODO: wire up youtube api request, to pass to SeachBar component, only one that needs to be aware of data
-    onTermSubmit = (term) => {
-        // can call instance of youtube apy you created
-        // pass in route that I want acces to, SEARCH ENDPOINT IN THIS CASE
-        youtube.get('/search', {
+    
+
+    // want to take list of videos received and set them as state on our APP component, will allow it to udpate, redender itself
+    //  allow us to render newly fetched videos onto the screen
+    onTermSubmit = async term => {
+        const resp = await youtube.get('/search', {
             params: {
                 q: term,
             }
         })
+        //  can see data in console => network => xhr => preview
+        // here is wher list of videos is being returned
+        console.log(resp.data.items);
+        
     }
     
     render() {

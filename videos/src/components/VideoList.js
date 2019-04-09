6c1043dcc6d3affeaba0a28886fdc48dbaa940fe
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-09 00:32:15 -0400

    adding funtional component of Vide List element

---
+++ videos/src/components/VideoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import VideoItem from './VideoItem'
+
+// add deconstructed prop
+const VideoList = ({ videos }) => {
+    // map over list of items
+    // inside of videos array are many objects,
+    // which we describe with video variable
+    // map over array to produce brand new array
+    const renderdList = videos.map((video)=>{
+        return <VideoItem />
+    });
+
+    return(
+        <div>
+            {renderdList}
+        </div>
+    )
+}
+
+export default VideoList

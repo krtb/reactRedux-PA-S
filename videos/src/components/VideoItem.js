cd0c0549988fe76f3d8a4f2f8457f293f679504a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-13 07:37:49 -0400

    destructure out prop from videolist, onVideoSelect callback

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,7 +1,8 @@
 import './VideoItem.css'
 import React from 'react';
 
-const VideoItem = ({video}) => {
+// destructure out prop being pass in from videlist, in videoItem parent component
+const VideoItem = ({video, onVideoSelect}) => {
     return(
         // find root div of component and give it className === to component
         <div className="video-item item" >

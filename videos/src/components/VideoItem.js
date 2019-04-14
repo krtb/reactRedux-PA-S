ba5918fcd0e1a9632f1928ed95a035f71b83f4fa
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-14 11:50:29 -0400

    add alt tag to img

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -9,7 +9,7 @@ const VideoItem = ({video, onVideoSelect}) => {
         // add arrow function, want to call onVideoSelect with some arg
         // if placed without arrow func, will be called without video object
         <div onClick={() => onVideoSelect(video)} className="video-item item" >
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content" >
                 <div className="header">
                     {video.snippet.title}

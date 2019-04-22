a158f220232d7973ee1ad7b0f46f49f1b685c08c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-22 15:33:05 -0400

    add SongDetail component to app file

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SongsList from './SongsList'
+import SongDetail from './SongDetail'
 
 
 const App = () => {
@@ -8,6 +9,9 @@ const App = () => {
                 <div className="ui row">
                     <div className="column eight wide" >
                         <SongsList />
+                    </div>
+                    <div className="column eight wide">
+                        <SongDetail/>
                     </div>
                 </div>
             </div>

746b1e4e05b0a88584a0fe6ae357aecb91bf5f6b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-22 13:42:27 -0400

    add action with type and payload to action creator, add duff way of exporting

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,4 +1,11 @@
 // ACTION CREATOR
-const selectSong = () => {
+export const selectSong = (song) => {
+    // REUTURN AN ACTION, as plain JS object
+    // payload prop is optional, type IS required
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    };
+    // assume that will pass in song arg passed in above
+};
 
-}

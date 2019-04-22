6de96246ba021a3eb23e1383f09ac5d5a1f15c39
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-22 14:58:31 -0400

    add callback func to renderList func, to show song being imported from Actions

--- songs/src/components/SongsList.js
+++ songs/src/components/SongsList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions' //exported without default
 
 class SongsList extends Component {
 
@@ -10,7 +11,9 @@ class SongsList extends Component {
             return (
                 <div className="item" key={song.title}  >
                     <div className="right floated content" >
-                        <button className="ui primary button" >
+                        <button className="ui primary button" 
+                            onClick={()=> this.props.selectSong(song)}
+                        >
                             Select
                         </button>
                     </div>
@@ -24,7 +27,7 @@ class SongsList extends Component {
     render(){
         // this.props === {songs: state.songs}
         // how we get data from our redux store into a component
-        console.log(this.props);
+        // console.log(this.props);
         return (
             <div className="ui divided list" >{this.renderList()}</div>
         )
@@ -35,7 +38,11 @@ class SongsList extends Component {
 // run calc on it, will cause data to show up as props inside this component
 // function could be called anything else, 'getMyState' for example
 // called with all state inside redux store
+
+// anytime rerun reducers and prod new state obj, this function runs with newly created state obj.
 const mapStateToProps = (state) => {
+    console.log(state);
+    
     return {
         songs: state.songs
     }
@@ -47,4 +54,7 @@ const mapStateToProps = (state) => {
 // tell it that we want to get a list of sonds out of our redux store from provider
 
 // configure connect component by passing in mapStateToProps, configed by passing in a function
-export default connect(mapStateToProps)(SongsList)
+// use ES2015 syntax to make one word of selectSong
+// this selectSong action creator will be passed in as a prop
+// will take action that gets returned and send it into the dispatch function - KEY THING TO KNOW
+export default connect(mapStateToProps, {selectSong})(SongsList)

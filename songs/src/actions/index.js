38c9b10084425900ba49107787b8119fd6b125da
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-22 14:56:39 -0400

    add comment on action being passed to reducer, selectedSongReducer

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,4 +1,7 @@
 // ACTION CREATOR
+
+// if we pass a song to this, will return an action type and our payload of the song
+// this actionshould end up in our selectedSongReducer, inside of our rediucers file
 export const selectSong = (song) => {
     // REUTURN AN ACTION, as plain JS object
     // payload prop is optional, type IS required

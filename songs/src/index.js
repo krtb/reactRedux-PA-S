e7804124bdbe50d3266e506411427f7f15efee1b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-22 14:06:08 -0400

    add redux and react-redux imports, wrap App in Provider component

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Provider is component from react-redux lib, components with capitals.
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+// 3rd party above and my own below
 import App from './components/App';
+import reducers from './reducers'
 
-// setup ReactDOM
+// need to create an instance of provider and wrap app component with it
 ReactDOM.render( <App />, document.querySelector('#root'));

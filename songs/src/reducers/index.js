fd0d8cab4935eba0578bc0820534a866ea909a18
Kurt Bauer <kurtbauer55@protonmail.com>
2019-04-22 13:53:45 -0400

    create redyer for selected song and for static list of songs

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,37 @@
+// reducer that will contain a static list of songs
+// usually would not need reducer for this, just an example
+const songsReducer = () => {
+    // will return an array of objects,
+    // each object is a different song
+    return [
+        {
+            title: 'No SCrubs',
+            duration: '4:05'
+        },
+        {
+            title: 'Macarena',
+            duration: '2:30 '
+        },
+        {
+            title: 'All Star',
+            duration: '3:15'
+        },
+        {
+            title: 'Backstreet Boys',
+            duration: '1:45'
+        }
+    ];
+}
+
+// default of null for selected song when app starts
+const selectedSongReducer = (selectedSong = null, action) => {
+    // if type is songSelected, return it's payload prop
+    // has payload song arg that user is trying to select
+
+    // wrote out if statement assuming that there will be other actions/types created in the future
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload;
+    }
+    // if not the correct type, return what ever currentyl selected song is
+    return selectedSong
+}

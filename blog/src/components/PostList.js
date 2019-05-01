f413459ff6a1112051668d1d64959c9970633a10
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-01 01:15:41 -0400

    create postList class component

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render(){
+        return(
+            <div>
+                Post list!
+            </div>
+        );
+    }
+}
+
+export default PostList

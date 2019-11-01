76d31c5724d81efc557cda04d908ea3034c5d3ed
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-01 15:36:05 -0400

    no longer returning an action, instead action-object

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,9 +4,6 @@ export const fetchPosts = () => {
     return function(dispatch, getState) {
         const promise = jsonPlaceholder.get('/posts');
 
-        return {
-            type: 'FETCH_POSTS',
-            payload: promise
-        };
+        dispatch({ type: 'FETCH_POSTS', payload: })
     };
 };

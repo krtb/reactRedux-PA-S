71c17be2c5c561eb65c66ff1de45a634f3c7712f
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-01 15:46:09 -0400

    define a function that returns a function

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,10 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = () => {
-    return function(dispatch, getState) {
-        const promise = jsonPlaceholder.get('/posts');
+// define a function that's going to return a function
+export const fetchPosts = () => 
+    async dispatch => {
 
-        dispatch({ type: 'FETCH_POSTS', payload: })
-    };
+    const response = await jsonPlaceholder.get('/posts');
+    
+    dispatch({ type: 'FETCH_POSTS', payload: response})
 };
